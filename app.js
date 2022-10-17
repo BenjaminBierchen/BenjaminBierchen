45c51cdc6999c8ca49e6333360c1a2b3deeffb19
dieelite6 <dieelite6@gmail.com>
2022-10-17 13:31:45 +0200

    hey

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-import swal from 'sweetalert';
-
     function myFunction() {
     var x = document.getElementById("text").value
             if (x == "42") {
-                swal("Gut gemacht!");
+                alert("Super")
             }
     }
