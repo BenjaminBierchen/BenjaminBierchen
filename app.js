0634a737301a8b949f8c576810498cc204b6e46c
dieelite6 <dieelite6@gmail.com>
2022-10-17 13:20:16 +0200

    funktionier doch jetzr

---
+++ app.js
@@ -0,0 +1,8 @@
+import swal from 'sweetalert';
+
+    function myFunction() {
+    var x = document.getElementById("text").value
+            if (x == "42") {
+                swal("Gut gemacht!");
+            }
+    }
