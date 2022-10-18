4fdd69ca71c9ed92893bf681434e8fb4d8207b34
dieelite6 <dieelite6@gmail.com>
2022-10-18 21:22:57 +0200

    almost

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-function pageRedirect() {
-  window.location.replace("http://www.google.com/");
-}      
 
-function checkAnswer() {
+  function checkAnswer() {
   var solutionInput = document.getElementById("text").value
-  
-  if (solutionInput == "42") {
-    setTimeout("pageRedirect()", 1000);
 
-  } else {
-    alert("das stimmt leider nicht")
+  if (solutionInput == "42") {
+    sleep(1000)
+    setTimeout(function pageRedirect() {
+      window.location.replace("loader.html");
+    });
   }
+  
 }
 
-/*function sleep(milliseconds) {
+function sleep(milliseconds) {
   const date = Date.now();
   let currentDate = null;
   do {
     currentDate = Date.now();
   } while (currentDate - date < milliseconds);
 }
-*/
+
