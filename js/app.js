251b8f88dd9a556902de1573b464a6ecf3fd9fea
dieelite6 <dieelite6@gmail.com>
2022-10-19 11:22:07 +0200

    hey

---
+++ js/app.js
@@ -0,0 +1,20 @@
+  function checkAnswer() { //func prüft Antwort 
+  var solutionInput = document.getElementById("text").value //uebernahme der eingabe des inputfeldes
+
+  if (solutionInput == "42") { //antwort pruefen 
+    sleep(1000) // delay 1000ms
+    setTimeout(function pageRedirect() {
+      window.location.replace("loader.html"); //weiterleitung auf congrats page
+    });
+  }
+  
+}
+
+function sleep(milliseconds) { //delay func 
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
