5478d62d1ce22bb7a4b4526f130ec6c2fb4b2cc4
dieelite6 <dieelite6@gmail.com>
2022-10-20 17:41:11 +0200

    hey

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
+
+  var count = 3; // variable für Versuche, am Anfang 3 
   function checkAnswer() { //func prüft Antwort 
-  var solutionInput = document.getElementById("text").value //uebernahme der eingabe des inputfeldes
-
-  if (solutionInput == "42") { //antwort pruefen 
-    sleep(1000) // delay 1000ms
-    setTimeout(function pageRedirect() {
-      window.location.replace("loader.html"); //weiterleitung auf congrats page
-    });
-  } else {
-
-
-  }
+    var solutionInput = document.getElementById("text").value //uebernahme der eingabe des inputfeldes
   
+    if (solutionInput == "42") { //antwort pruefen 
+      sleep(1000) // delay 1000ms
+      setTimeout(function pageRedirect() { //ueberarbeiten  
+        window.location.replace("loader.html"); //weiterleitung auf congrats page
+        });
+    } else {
+        count -=1;
+        document.getElementById("clicks").innerHTML = count;
+          if(count == 0) {
+            alert("Tipp")
+            location.reload();
+          }
+    }
+    document.getElementById("text").value = null; //input feld clear
 }
 
 function sleep(milliseconds) { //delay func 
   const date = Date.now();
   let currentDate = null;
-  do {
+    do {
     currentDate = Date.now();
   } while (currentDate - date < milliseconds);
 }
-
-  function counter() {
-    clicks +=1;
-    document.getElementById("clicks").innerHTML = clicks;
-  }
